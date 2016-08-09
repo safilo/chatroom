645439cdffb5263f136c1fc8a9fccd80214bd713
safilo <wulinqing@hotmail.com>
2016-08-09 22:45:29 +0800

    recover bower.json

--- util.js
+++ util.js
@@ -2,4 +2,3 @@ var crypto = require('crypto');
 exports.md5 = function(source){
     return crypto.createHash('md5').update(source).digest('hex');
 }
-console.log(exports.md5('byclear@foxmail.com'));
